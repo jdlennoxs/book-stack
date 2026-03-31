71194289b3ab358a19376215585ecc209aa0d900
Jack Scott <jack.scott10@gmail.com>
2026-03-31 22:25:03 +0100

    chore: improve vite bundling

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { visualizer } from 'rollup-plugin-visualizer'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
-})
+  plugins: [
+    tailwindcss(),
+    react(),
+    visualizer({ open: true }) // This will open a chart in your browser after the build
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Put the heavy hitters in their own file!
+          'graphics-engines': ['three', '@react-three/fiber', '@react-three/drei'],
+          'physics': ['@react-three/rapier'],
+        },
+      },
+    },
+  },
+} as any)
